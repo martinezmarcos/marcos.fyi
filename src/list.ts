b6ea2df8d8bb3fcbfe36d2463fc8809b447948cf
martinezmarcos <mmartinez@uplift.com>
2023-07-04 21:17:41 -0700

    MM Fix small bug, format

--- src/list.ts
+++ src/list.ts
@@ -1,9 +1,6 @@
-
 export function setupList(ul: HTMLUListElement) {
   const items = ul.getElementsByTagName("li");
   for (let i = 0; i < items.length; i++) {
     items[i].classList.add("underline", "decoration-2", "decoration-blue-400");
   }
 }
-
-
