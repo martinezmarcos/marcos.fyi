976ecdaadd616ff923fef1152055d16b8617079f
martinezmarcos <mmartinez@uplift.com>
2023-07-04 21:18:08 -0700

    MM Update pnpm

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          50: "#FFEDD8",
+          100: "#F3D5B5",
+          200: "#E7BC91",
+          300: "#D4A276",
+          400: "#BC8A5F",
+          500: "#A47148",
+          600: "#8B5E34",
+          700: "#6F4518",
+          800: "#603808",
+          900: "#583101",
+        },
+      },
+    },
   },
   plugins: [],
 };
